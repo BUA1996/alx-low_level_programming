216473ebc7806e31a102785426a7b6748bde0283
bua <buaisaac17@gail.com>
2023-06-18 03:52:38 -0700

    modified

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
-/**Mian - Function that prints alphabetical leters
-*Return: always 0
-*/
+/**
+ * main - entry point of the function function that prints alphabetical
+ * Return: always 0
+ */
 int main(void)
 {
 	char n;
 
 	for (n = 'a'; n <= 'z'; n++)
+	{
 		putchar(n);
+	}
 	for (n = 'A'; n <= 'Z'; n++)
+	{
 		putchar(n);
+	}
 	putchar('\n');
 	return (0);
 }
