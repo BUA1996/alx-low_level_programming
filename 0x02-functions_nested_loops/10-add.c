65a369aa1f753f69924848b1c15a6b9b0327034a
bua <buaisaac17@gail.com>
2023-06-20 09:07:34 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - function that adds two integers
+ * @p: int to be added to q
+ * @q: int to be added to p
+ * Return: sum of p and q
+ */
+int add(int p, int q)
+{
+	int sum;
+
+	sum = p + q;
+	return (sum);
+}
