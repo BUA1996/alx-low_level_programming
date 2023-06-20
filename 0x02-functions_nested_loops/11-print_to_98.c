1010d2e0e207f9afa03796e01bca79f69ebff81c
bua <buaisaac17@gail.com>
2023-06-20 13:20:57 -0700

    real numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - printing all natural numbers
+ * @n: printing from number n
+ */
+void print_to_98(int n)
+{
+	int p, q;
+
+	if (n <= 98)
+	{
+		for (p = n; p <= 98; p++)
+		{
+			if (p != 98)
+				printf("%d\n", p);
+			else if (p == 98)
+				printf("%d\n", p);
+		}
+	}
+	else if (n >= 98)
+	{
+		for (q = n; q >= 98; q--)
+		{
+			if (q != 98)
+				printf("%d\n", q);
+			else if (q == 98)
+				printf("%d\n", q);
+		}
+	}
+}
