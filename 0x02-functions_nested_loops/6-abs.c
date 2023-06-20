7612126bfecc1369ad12be87062641311ce428e4
bua <buaisaac17@gail.com>
2023-06-20 07:33:42 -0700

    absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * Return: always an absolute value of an integer
+ */
+int _abs(int p)
+{
+	if (p >= 0)
+	{
+		return (p);
+	}
+	else
+	{
+		return (-p);
+	}
+	_putchar('\n');
+}
