ddf803a8053abf219d879f0fcf8faad86ab5d1e5
bua <buaisaac17@gail.com>
2023-06-22 13:43:34 -0700

    written a function for multiplying two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - entry point of the function to multiply two numbers
+ * Return: always 0 or multiple
+ *@a: number to multiply to b
+ *@b: number to multiply with a
+ */
+int mul(int a, int b)
+{
+	int mult;
+
+	mult = a * b;
+	return (mult);
+}
