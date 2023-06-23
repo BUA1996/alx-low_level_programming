3d55da765a24c834ef4f4b53df8195d9027b8f9f
bua <buaisaac17@gail.com>
2023-06-23 14:05:12 -0700

    written a code to implement the fizzbuzz quiz

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - entry point of the function
+ * @size: size of the square
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int p, q;
+
+		for (p = 0; p < size; p++)
+		{
+			for (q = 0; q < size; q++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point of the function
+ * Return: always 0
+ */
+int main(void)
+{
+	int p;
+
+	for (p = 1; p <= 100; p++)
+	{
+		if (p % 3 == 0 && p % 5 != 0)
+		{
+			printf(" Fizz");
+		}
+		else if (p % 5 == 0 && p % 3 != 0)
+		{
+			printf(" Buzz");
+		}
+		else if (p % 3 == 0 && p % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (p == 1)
+		{
+			printf("%d", p);
+		}
+		else
+		{
+			printf(" %d", p);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
