dbdc8f6db384193727e3623b395140d8c703dc06
bua <buaisaac17@gail.com>
2023-06-23 14:49:53 -0700

    written a script to compute the prime factrs

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - entry point of the function
+ * Return; always 0
+ */
+int main(void)
+{
+	long int max;
+	long int p, q;
+
+	p = 612852475143;
+	max = -1;
+
+	while (p % 2 == 0)
+	{
+		max = 2;
+		p /= 2;;
+	}
+
+	for (q = 3; q <= sqrt(p); q = q + 2)
+	{
+		while (p % q == 0)
+		{
+			max = q;
+			p = p / q;
+		}
+	}
+	if (p > 2)
+		max = p;
+	printf("%ld\n", max);
+
+	return (0);
+}
