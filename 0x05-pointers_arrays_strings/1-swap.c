a5161d4c803ee38fcf96b876f862bd81662ee2d4
bua <buaisaac17@gail.com>
2023-06-26 02:40:16 -0700

    added the script to swap values in the file 1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps in two values
+ * @a: value to be swapped with b
+ * @b: value to be swapped by a
+ * Return: the swapped values
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
