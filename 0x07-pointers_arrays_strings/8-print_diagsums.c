5c49d0ec17b03fa6bfd0f069d7ba005007341e28
bua <buaisaac17@gail.com>
2023-07-03 14:24:27 -0700

    function that prints the sum of the diagnal of a matrix

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - function to print the sum of a diagnals of a square matrix
+ * @a: input
+ * @size: input
+ * Return: Always 0
+ */
+void print_diagsums(int *a, int size)
+{
+	int sum1, sum2, p;
+
+	sum1 = 0;
+	sum2 = 0;
+
+	for (p = 0; p < size; p++)
+	{
+		sum1 = sum1 + a[p * size + p];
+	}
+	for (p = size - 1; p >= 0; p--)
+	{
+		sum2 += a[p * size + (size - p - 1)];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
