ec06ca614115dd73d28eff2b2b20cb99d9295a5d
bua <buaisaac17@gail.com>
2023-07-20 02:43:05 -0700

    created the files for task 1-print and addeda script that prints a number followed by a comma

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function that Prints numbers
+ * @separator: string to seperate the numbers
+ * @n: number of integers passed
+ * @...: ellipse
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	
+	va_list number;
+
+	va_start(number, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(number, int));
+
+		if (i != (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(number);
+}
